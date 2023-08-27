b0e4634ac8864c6e3479f1ee6847f47a30b6a3b2
Lucas Moore <14173177@student.uts.edu.au>
2023-08-27 18:35:04 +1000

    u

---
+++ main/CMakeFiles/ros_interact_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_interact_node.dir/src/ros_interact.cpp.o"
+  "devel/lib/ros_interact/ros_interact_node"
+  "devel/lib/ros_interact/ros_interact_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros_interact_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
